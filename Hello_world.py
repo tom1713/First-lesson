c23b0a1af6883db0682298fbd5de9011f859c436
Kevin Li <likevintw@gmail.com>
2022-02-13 21:26:28 +0800

    add .gitignore and README.md, ok

--- Hello_world.py
+++ Hello_world.py
@@ -1,1 +1,13 @@
-print('hellow world')
+
+
+def is_prime_number(number) -> bool:
+    return True
+
+
+if __name__ == '__main__':
+
+    if not is_prime_number(43):
+        print("error, 43 is a prime")
+
+    if is_prime_number(33):
+        print("error, 33 is not a prime")
