49e5e0f71cc54584ca835f931b3e2a9d2eb51a4b
Kevin Li <likevintw@gmail.com>
2022-02-18 14:00:06 +0800

    add unit test, ok

---
+++ test_is_prime.py
@@ -0,0 +1,35 @@
+import unittest
+
+import prime_examer
+
+# python3 -m unittest -v <test_example.y>
+
+
+class UnitTest(unittest.TestCase):
+
+    def test_is_prime(self):
+        class Tester:
+            def __init__(self, args, wants) -> None:
+                self.args = args
+                self.wants = wants
+
+        testers = []
+
+        testers.append(Tester(123, False))
+        testers.append(Tester(223, True))
+        testers.append(Tester(117, False))
+        testers.append(Tester(613, True))
+        testers.append(Tester(143, False))
+        testers.append(Tester(846, False))
+
+        for tester in testers:
+            try:
+                self.assertEqual(prime_examer.is_prime(
+                    tester.args), tester.wants)
+            except:
+                print("result of {} is not {}".format(
+                    tester.args, tester.wants))
+
+
+if __name__ == '__main__':
+    unittest.main()
